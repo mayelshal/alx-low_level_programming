3c76b9aaf17907e79630012d1447c666760a9da0
mayelshal <mayelshal1810@gmail.com>
2023-04-20 11:49:32 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
-int main (void){
-   puts("\"Programming is like building a multilingual puzzle");
+
+/**
+ * main-entry point
+ *
+ * descrepition : A C programming that prints with puts function
+ *
+ * return : always retursn(0) success */
+
+int main (void)
+{		
+  puts("\"Programming is like building a multilingual puzzle");
    return(0);
 }
