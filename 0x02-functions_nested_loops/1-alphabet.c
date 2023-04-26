6ce32b1dc60b8db26f0b0484273c13a754c24c33
mayelshal <mayelshal1810@gmail.com>
2023-04-26 12:03:19 -0700

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,11 +5,12 @@
  *                  the alphabet a - z
 */
 
-void print_alphabet(void);
-
-       int ch;
-
-       for (ch = 'a' ; ch <= 'z'; ch++)
-               _putchar(ch);
+void print_alphabet(void)
+{
+       char c;
+       for (c = 'a' ; c <= 'z'; c++)
+       {
+               _putchar(c);
+       }
        _putchar('\n');
 } 
