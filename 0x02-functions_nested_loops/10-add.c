46345164bf5d40616f96046b0c79880fd2294ce7
mayelshal <mayelshal1810@gmail.com>
2023-04-26 13:53:41 -0700

    a

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * ass - adds two values
+ * @n1: the first num to be added
+ * @n2: te second num to be added
+ * Retuen: Intger value
+ */
+
+int add(int n1, int n2)
+
+{
+	return (n1 + n2);
+}	
